a100dc78b7f339f4c9542733577385e6d59309b8
Shusuke <s1420630@u.tsukuba.ac.jp>
2014-06-13 19:16:39 +0900

    DualCameraにファイルを追加。 practiceシーンを新たに作成。

---
+++ Assets/Script/PrintDisplayData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintDisplayData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (Display.displays[0].systemHeight);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/SetResolution.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetResolution: MonoBehaviour {
+	public int ScreenWidth;
+	public int ScreenHeight;
+	void Awake()
+	{
+		// PC向けビルドだったらサイズ変更
+		if (Application.platform == RuntimePlatform.WindowsPlayer ||
+		    Application.platform == RuntimePlatform.OSXPlayer ||
+		    Application.platform == RuntimePlatform.LinuxPlayer )
+		{
+			Screen.SetResolution(ScreenWidth, ScreenHeight, false);
+		}
+	}
+	void Start () {
+	}
+	
+	void Update () {
+	}
+}
